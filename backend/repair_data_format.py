ddfee409fd28e3669d289b08d0ee3b9711636d69
EdgarElizondo <edgar_7_23@hotmail.com>
2024-08-04 18:06:56 -0600

    Modified data format to delete white spaces in columns

--- backend/repair_data_format.py
+++ backend/repair_data_format.py
@@ -1,23 +1,26 @@
 import glob
 
-files = glob.glob("backend\\src\\wheather_data\\TG_*")
 
-for file in files:
-    break
+def change_data_format(main_path):
+    # Get File
+    with open(file, "r", encoding="utf8") as f:
+        text = f.readlines()
 
-# Get File
-with open(files[0], "r") as file:
-    text = file.readlines()
+    # Get data starting line
+    num_line = 0
+    for line in text:
+        if line[:5] == "STAID":
+            break
+        num_line +=1
+    # Delete white spaces
+    text[num_line] = text[num_line].replace(" ", "")
+    # Save Changes
+    with open(file, "w", encoding="utf8") as f:
+        f.writelines(text)
 
-# Get data starting line
-num_line = 0
-for line in text:
-    if line[:5] == "STAID":
-        break
-    num_line +=1
-# Delete white spaces
-text[20] = text[20].replace(" ", "")
-
-# Save Changes
-with open(files[0], "w") as file:
-    file.writelines(text)
+if __name__ == "__main__":
+    files = glob.glob("backend\\src\\wheather_data\\TG_*")
+    for index,file in enumerate(files):
+        if index % 50 == 0:
+            print(f"Number of Files modified: {index}")
+        change_data_format(file)
